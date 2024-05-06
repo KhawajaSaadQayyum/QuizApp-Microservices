783a18cfdc73f461ce6a405e6f48763210ae1692
Saad Qayyum <khawajasaadqayyum4110@gmail.com>
2024-05-06 15:00:46 +0500

    This is spring Boot Application

---
+++ src/main/java/QuizAppMicroservices/QuizAppMicroservicesApplication.java
@@ -0,0 +1,14 @@
+package QuizAppMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppMicroservicesApplication.class, args);
+		System.out.println("Welcome to the Microservices Learning");
+	}
+
+}
---
+++ src/test/java/QuizAppMicroservices/QuizAppMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package QuizAppMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
